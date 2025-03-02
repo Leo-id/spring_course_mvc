7781af4b7e8760477beba5471e5e770eb03520f3
Константин Леончик <k.leonchik@inbox.ru>
2025-03-03 00:25:44 +0300

    end 4_68

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.Pattern;
 
 
 @Controller
+//@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -23,13 +24,17 @@ public class MyController {
         Employee emp=new Employee();
         emp.setName("Ivan");
         emp.setSurname("Ivanov");
+        emp.setEmail("Ivanov@abc.com");
+        emp.setPhoneNumber("999-99-99");
+        emp.setSurname("000");
         model.addAttribute("employee", emp);
         return "ask-emp-details-view";
     }
 
     @PostMapping("/showDetails")
     public String showEmpDetails(@Valid @ModelAttribute("employee")Employee employee, BindingResult bindingResult) {
-
+//String name=employee.getName();
+//employee.setName("MR. "+name);
        if(bindingResult.hasErrors()){
            return "ask-emp-details-view";
        }
